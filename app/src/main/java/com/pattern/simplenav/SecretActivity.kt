2323f3275d2e27a4cdea4b9e7d72ae08cee93701
gobbledyg00k <114208752+gobbledyg00k@users.noreply.github.com>
2024-10-14 13:05:19 +0700

    add singleTask mode

--- app/src/main/java/com/pattern/simplenav/SecretActivity.kt
+++ app/src/main/java/com/pattern/simplenav/SecretActivity.kt
@@ -1,6 +1,8 @@
 package com.pattern.simplenav
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -15,6 +17,12 @@ class SecretActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        val button = findViewById<Button>(R.id.button4)
+        button.setOnClickListener{
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
         }
     }
 }
