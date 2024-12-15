a94aab6ac26bcb555a37c4caa41ca88e407d970f
gobbledyg00k <114208752+gobbledyg00k@users.noreply.github.com>
2024-12-15 20:46:53 +0700

    added the value of the currency to list

--- Converter/app/src/main/java/com/pattern/converter/ListActivity.kt
+++ Converter/app/src/main/java/com/pattern/converter/ListActivity.kt
@@ -6,6 +6,8 @@ import android.widget.ArrayAdapter
 import android.widget.ListView
 import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
+import java.math.BigDecimal
+import java.math.RoundingMode
 
 class ListActivity : AppCompatActivity() {
 
@@ -17,13 +19,20 @@ class ListActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_list)
         val currencyListView = findViewById<ListView>(R.id.currencyListView)
+
         val bundle = intent.extras
         if (bundle != null) {
             currencyMap = bundle.getSerializable("currencyMap", HashMap::class.java) as MutableMap<String, Double>
             currencies = bundle.getStringArrayList("currencies") ?: mutableListOf()
         }
 
-        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, currencies)
+        val displayList = currencies.map { currency ->
+            val value = currencyMap[currency] ?: 0.0
+            val roundedValue = BigDecimal(value).setScale(2, RoundingMode.HALF_UP)
+            "$currency: $roundedValue RUB"
+        }
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, displayList)
         currencyListView.adapter = adapter
     }
 }
